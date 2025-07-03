8572f2443982e31dae9cc91c0f8f565a9d4de4a4
LeganSar44 <hoangslevan@gmail.com>
2025-07-03 16:44:24 +0700

    optimize code js to tsx

---
+++ client/components/Layout.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import Navbar from "./Navbar";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>TaskRiser</title>
+        <meta
+          name="description"
+          content="TaskRiser - Your Task Management System"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="min-h-screen bg-gradient-to-br from-[#0077b6] via-[#00b4d8] to-[#90e0ef] text-[#0077b6] font-['Orbitron',_sans-serif] backdrop-blur-xl">
+        <Navbar />
+        <main className="container mx-auto px-4 py-8">{children}</main>
+      </div>
+    </>
+  );
+}
